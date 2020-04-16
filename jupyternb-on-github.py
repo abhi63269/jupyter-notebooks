a340fff2bc6f1ba12795243d8bca4d0eecdbd465
Abhishek Jain <abhi63269@gmail.com>
2020-04-16 15:47:24 +0530

    python converted nb

---
+++ jupyternb-on-github.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Jupter notebook on github
+
+# ## Some sample code text area...
+
+# In[2]:
+
+
+get_ipython().system('pip3 show h2o')
+
+
+# In[3]:
+
+
+a = 'hello world'
+print(a)
+
+
+# In[1]:
+
+
+for x in range(0,5):
+    print(x, end=' ')
+
+
+# In[ ]:
+
+
+
+
